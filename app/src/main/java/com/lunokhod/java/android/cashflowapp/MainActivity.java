675331c3d9082bfcf22ccf5c2d464036aa33494a
Alexey Astakhov <aastv@yandex.ru>
2015-12-31 16:54:22 +0300

    Initial Commit

---
+++ app/src/main/java/com/lunokhod/java/android/cashflowapp/MainActivity.java
@@ -0,0 +1,65 @@
+package com.lunokhod.java.android.cashflowapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Spinner categorySpinner;
+    private ArrayAdapter<String> spinnerAdapter;
+    private String[] array = { "Барсик", "Мурзик", "Васька", "Рыжик", "Барсик1", "Мурзик1", "Васька1",
+            "Рыжик1", "Барсик2", "Мурзик2", "Васька2", "Рыжик2", "Барсик3", "Мурзик3", "Васька3", "Рыжик3"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        categorySpinner = (Spinner)findViewById(R.id.categorySpinner);
+        //spinnerAdapter = new ArrayAdapter<String>(this, R.layout.spinner_layout, R.id.spinTextView, array);
+        spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, array);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categorySpinner.setAdapter(spinnerAdapter);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
