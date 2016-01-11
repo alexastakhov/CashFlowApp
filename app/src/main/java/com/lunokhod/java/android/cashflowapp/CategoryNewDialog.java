0fd506d59b6ae448e1f2421a74fde683fc1059a2
Alexey Astakhov <aastv@yandex.ru>
2016-01-11 18:51:13 +0300

    Added Error Text

--- app/src/main/java/com/lunokhod/java/android/cashflowapp/CategoryNewDialog.java
+++ app/src/main/java/com/lunokhod/java/android/cashflowapp/CategoryNewDialog.java
@@ -9,11 +9,16 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 /**
  * Created by alex on 10.01.2016.
  */
 public class CategoryNewDialog extends DialogFragment {
+
+    private int initHeigh;
+    private int initWidth = 350;
 
     public static CategoryNewDialog getInstance() {
         CategoryNewDialog dialog = new CategoryNewDialog();
@@ -27,12 +32,10 @@ public class CategoryNewDialog extends DialogFragment {
         final View view = inflater.inflate(R.layout.category_new_dialog, container, false);
 
         getDialog().setTitle(R.string.category_dialog_header2);
+        initHeigh = getDialog().getWindow().getAttributes().height;
+        getDialog().getWindow().setLayout(initWidth, initHeigh);
 
-        int dHeigt = getDialog().getWindow().getAttributes().height;
-        int dWidth = 350;
-        getDialog().getWindow().setLayout(dWidth, dHeigt);
-
-        EditText editText = (EditText)view.findViewById(R.id.categoryEditText);
+        final EditText editText = (EditText)view.findViewById(R.id.categoryEditText);
         CheckBox checkBox = (CheckBox)view.findViewById(R.id.categoryPrioCheckBox);
         Button saveBtn = (Button)view.findViewById(R.id.categorySaveButton);
         Button cancelBtn = (Button)view.findViewById(R.id.categoryCancelButton);
@@ -40,7 +43,12 @@ public class CategoryNewDialog extends DialogFragment {
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                closeDialog();
+                if (editText.getText().length() == 0)
+                    showNameErrorText(view, true);
+                else {
+                    showNameErrorText(view, false);
+                    closeDialog();
+                }
             }
         });
 
@@ -64,5 +72,22 @@ public class CategoryNewDialog extends DialogFragment {
 
     private void deleteCategory(String categoryName) {
         ((CategoryActivity)getActivity()).deleteCategory(categoryName);
+    }
+
+    private void showNameErrorText(View view, boolean show) {
+        TextView errorText = (TextView)view.findViewById(R.id.errorTextView);
+        //int tvHeihgt = errorText.getHeight();
+        int mHeight = ((LinearLayout.LayoutParams)errorText.getLayoutParams()).topMargin;
+        int tvHeihgt = ((LinearLayout.LayoutParams)errorText.getLayoutParams()).height;
+
+        errorText.setText(R.string.category_dialog_error_name_text);
+        if (show) {
+            getDialog().getWindow().setLayout(initWidth, initHeigh + tvHeihgt + mHeight);
+            errorText.setVisibility(View.VISIBLE);
+        }
+        else {
+            getDialog().getWindow().setLayout(initWidth, initHeigh);
+            errorText.setVisibility(View.GONE);
+        }
     }
 }
